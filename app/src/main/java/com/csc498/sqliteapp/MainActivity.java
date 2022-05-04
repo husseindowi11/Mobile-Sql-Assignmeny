dda42d4140eb8f5642a54a5bc96c8e2e1e66f03c
husseinadkerning <hseneldowi@gmail.com>
2022-05-04 22:05:40 +0300

    add sql logic

--- app/src/main/java/com/csc498/sqliteapp/MainActivity.java
+++ app/src/main/java/com/csc498/sqliteapp/MainActivity.java
@@ -2,13 +2,58 @@ package com.csc498.sqliteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        data = new ArrayList<>();
+
+
+        try{
+
+            SQLiteDatabase sql = this.openOrCreateDatabase("exams_db", MODE_PRIVATE, null);
+            sql.execSQL("CREATE Table IF NOT EXISTS exams(name VARCHAR, date Date)");
+//        sql.execSQL("INSERT INTO exams(name,date) VALUES('Mobile Computing','2022-02-09')");
+//        sql.execSQL("INSERT INTO exams(name,date) VALUES('Andalusian','2022-02-09')");
+
+            Cursor cursor = sql.rawQuery("SELECT * FROM exams", null);
+            int name_index = cursor.getColumnIndex("name");
+            int date_index = cursor.getColumnIndex("date");
+
+            cursor.moveToFirst();
+
+            while (cursor != null) {
+                String text = cursor.getString(name_index) + " on " + cursor.getString(date_index);
+
+                data.add(text);
+
+                cursor.moveToNext();
+            }
+
+
+
+        }catch(
+                Exception e)
+
+        {
+            e.printStackTrace();
+        }
+
+
     }
+
+
 }
